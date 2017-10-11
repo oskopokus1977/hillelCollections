81a625c8d87f548a751305fab1b95665b4ecec8d
oskopokus1977 <oskopokus1977@gmail.com>
2017-10-11 23:21:38 +0300

    Tests

--- src/test/java/TestMultiSet.java
+++ src/test/java/TestMultiSet.java
@@ -9,7 +9,7 @@ public class TestMultiSet {
 
     @Test
     public void addTest() {
-        MultiSet<Integer> multiSet = new MultiSet<Integer>();
+       MultiSet<Integer> multiSet = new MultiSet<Integer>();
         multiSet.add(1);
         multiSet.add(2);
         multiSet.add(3);
@@ -19,4 +19,29 @@ public class TestMultiSet {
         Assert.assertTrue((multiSet.hashMap.get(3) == 2));
     }
 
+    @Test
+    public void removeTest(){
+        MultiSet<String> multiSet = new MultiSet<String>();
+        multiSet.add("Test1");
+        multiSet.add("Test2");
+        multiSet.add("Test3");
+        multiSet.add("Test1");
+
+        Assert.assertTrue((multiSet.hashMap.get("Test1") == 2));
+        multiSet.remove("Test1");
+        Assert.assertTrue((multiSet.hashMap.get("Test1") == 1));
+        multiSet.remove("Test2");
+        multiSet.remove("Test2");
+    }
+
+    @Test
+    public void containsTest(){
+        MultiSet<Character> multiSet = new MultiSet<Character>();
+        multiSet.add('J');
+        multiSet.add('A');
+        multiSet.add('V');
+        multiSet.add('A');
+        Assert.assertEquals(multiSet.contains('A'),2);
+        Assert.assertEquals(multiSet.contains('X'),0);
+    }
 }
